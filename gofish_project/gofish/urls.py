10c9c9365daa6fa3d14b9562474f7326d549d00f
Maksim Solovjov <himaxx2@gmail.com>
2014-10-01 00:38:28 +0100

    Outline the API to develop for the main game functionality

--- gofish_project/gofish/urls.py
+++ gofish_project/gofish/urls.py
@@ -2,4 +2,19 @@ from django.conf.urls import patterns, url
 from gofish import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        # start a new game round
+        url(r'^api/start/(?P<level>\w+)/$', views.start, name='start'),
+        # end the current game round preemptively
+        url(r'^api/end/$', views.end, name='end'),
+        # execute action
+        url(r'^api/action/(?P<action>\w+)/(?P<par>[\w,]+)/$', views.action, name='action'),
+        # update something (buy it)
+        url(r'^api/update/(?P<target>\w+)/$', views.update, name='update'),
+        # switch modifier
+        url(r'^api/choose/(?P<modifier>\w+)/$', views.choose, name='choose'),
+        # buy modifier
+        url(r'^api/buy/(?P<modifier>\w+)/$', views.buy, name='buy'),
+        # get game info (available levels, etc)
+        url(r'^api/getgame/$', views.getgame, name='getgame'),
+        )
--- gofish_project/gofish/views.py
+++ gofish_project/gofish/views.py
@@ -1,4 +1,33 @@
 from django.http import HttpResponse
+import json
 
 def index(request):
     return HttpResponse("Gofish says hello world!")
+
+def start(request, level):
+    response = {'level': level}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def end(request):
+    response = {}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def action(request, action, par):
+    response = {'action': action, 'par': par}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def update(request, target):
+    response = {'target': target}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def choose(request, modifier):
+    response = {'modifier': modifier}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def buy(request, modifier):
+    response = {'modifier': modifier}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+def getgame(request):
+    response = {}
+    return HttpResponse(json.dumps(response), content_type="application/json")
