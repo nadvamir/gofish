bd4ce80386e80ddefd6fd0071d45eb3ef1063baa
Sean Jakobsen <skwjakobsen@gmail.com>
2014-10-05 20:45:06 +0100

    Added template, static and media directories. Added some test images and a very simple template example.

--- gofish_project/gofish/views.py
+++ gofish_project/gofish/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 import json
 
 def index(request):
-    return HttpResponse("Gofish says hello world!")
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('gofish/index.html', context_dict, context)
 
 def start(request, level):
     response = {'level': level}
