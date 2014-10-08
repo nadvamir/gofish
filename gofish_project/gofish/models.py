7eaf67682d518686a34a2e100953be51f7a718cb
Maksim Solovjov <himaxx2@gmail.com>
2014-10-08 22:41:33 +0100

    Add some basic models

--- gofish_project/gofish/models.py
+++ gofish_project/gofish/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# a player of the game
+class Player(models.Model):
+    user      = models.OneToOneField(User)
+
+    # how much currency this player has
+    money     = models.IntegerField(default=10)
+    # what updates has it bought (JSON)
+    updates   = models.TextField()
+    # what modifiers does it has (JSON)
+    modifiers = models.TextField()
+
+    def __unicode__(self):
+        return self.user.username + ' player'
+
+# a game class, representing the current level and everything
+# that happens in it
+class Game(models.Model):
+    player = models.OneToOneField(Player)
+    # a json representation of the current level
+    level  = models.TextField()
+    # a json representation of the fish caught
+    caught = models.TextField()
+    
+    def __unicode__(self):
+        return self.player.user.username + ' game'
--- gofish_project/gofish_project/urls.py
+++ gofish_project/gofish_project/urls.py
@@ -10,11 +10,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'gofish_project.views.home', name='home'),
     # url(r'^gofish_project/', include('gofish_project.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^gofish/', include('gofish.urls')),
 )
