1a3bd908fbef8e872963d11a3028138573e365da
Sean Jakobsen <skwjakobsen@gmail.com>
2014-10-26 21:47:35 +0000

    More Angular-ness. Added bootstrap heavy header in its own directive. Moved level select to its own directive.

---
+++ phonegap/gofish/www/js/directives/header.js
@@ -0,0 +1,13 @@
+goFish.directive("header", [function(){
+
+	return {
+		restrict: "E",
+		templateUrl: "./partials/header.html",
+		scope: {},
+		controller: function() {
+
+		},
+		controllerAs: "headerCtrl"
+	};
+
+} ]);
---
+++ phonegap/gofish/www/js/directives/levelSelect.js
@@ -0,0 +1,32 @@
+goFish.directive("levelSelect", [function(){
+
+	return {
+		restrict: "E",
+		templateUrl: "./partials/levelSelect.html",
+		scope: {},
+		controller: function($http) {
+
+			this.updateGame = function() {
+				$http.get("http://nadvamir.pythonanywhere.com/gofish/api/getgame/").
+					success(function(data, scope) {
+						if(data.error) {
+							alert(data.error);
+						}
+						else {
+							controller.game = data;
+						};
+					}).
+					error(function() {
+						alert("Error getting game.");
+					})
+			};
+
+			this.game = {};
+			var controller = this;
+			this.updateGame();
+
+		},
+		controllerAs: "lsCtrl"
+	};
+
+} ]);
