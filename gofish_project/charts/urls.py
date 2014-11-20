bfeffe98ae1f98e65083f316cd0ab71a0f814f48
Maksim Solovjov <himaxx2@gmail.com>
2014-11-20 10:11:45 +0000

    Create chart admin panel skeleton structure

--- gofish_project/charts/urls.py
+++ gofish_project/charts/urls.py
@@ -2,5 +2,17 @@ from django.conf.urls import patterns, url
 from charts import views
 
 urlpatterns = patterns('',
+        # index page, to show the options
         url(r'^$', views.index, name='index'),
+        # consumes log and creates database entries from it
+        url(r'^parse_log/$', views.parseLog, name='parseLog'),
+        # visualiser of individual user data
+        url(r'^data_by_user/$', views.dataByUser,
+            name='dataByUser'),
+        # visualiser of aggregated data
+        url(r'^data_aggregated/$', views.dataAggregated,
+            name='dataAggregated'),
+        # api call to get specific data (TODO)
+        url(r'^api/get_data/$', views.getData,
+            name='getData'),
 )
--- gofish_project/charts/views.py
+++ gofish_project/charts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import user_passes_test
+import json
 
 #################################################################
 # CHART ADMIN
@@ -19,3 +20,26 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('charts/index.html', context_dict, context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def parseLog(request):
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('charts/log_parsed.html', context_dict, context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def dataByUser(request):
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('charts/data_user.html', context_dict, context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def dataAggregated(request):
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('charts/data_aggregated.html', context_dict, context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def getData(request):
+    response = {}
+    return HttpResponse(json.dumps(response), content_type="application/json")
