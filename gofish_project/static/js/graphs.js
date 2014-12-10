1688fe3f7bd424eca5aa5620d8e73c85a2a024da
Maksim Solovjov <himaxx2@gmail.com>
2014-12-10 15:02:39 +0000

    Remove commented unnecessary code

--- gofish_project/static/js/graphs.js
+++ gofish_project/static/js/graphs.js
@@ -1,9 +1,6 @@
 // Load the Visualization API and the piechart package.
 google.load('visualization', '1.0', {'packages':['corechart']});
 
-// Set a callback to run when the Google Visualization API is loaded.
-//google.setOnLoadCallback(drawChart);
-
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
