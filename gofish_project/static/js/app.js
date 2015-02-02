492b929750f5396df5e5b8be913b46b451cfe487
Maksim Solovjov <himaxx2@gmail.com>
2015-02-02 17:20:26 +0000

    Try out mithril example

--- gofish_project/static/js/app.js
+++ gofish_project/static/js/app.js
@@ -1,11 +1,48 @@
-var b, f;
+var app;
 
-f = function() {
-  return 'stuff';
+app = {};
+
+app.PageList = function() {
+  return m.prop([
+    {
+      title: "Getting Started",
+      url: "getting-started.html"
+    }, {
+      title: "Documentation",
+      url: "mithril.html"
+    }, {
+      title: "Mithril Blog",
+      url: "http://lhorie.github.io/mithril-blog/"
+    }, {
+      title: "Mailing List",
+      url: "https://groups.google.com/forum/#!forum/mithriljs"
+    }
+  ]);
 };
 
-b = function() {
-  return 'moar';
+app.controller = function() {
+  var pages;
+  pages = app.PageList();
+  return {
+    pages: pages,
+    rotate: function() {
+      return pages().push(pages().shift());
+    }
+  };
 };
+
+app.view = function(ctrl) {
+  return [
+    ctrl.pages().map(function(page) {
+      return m("a", {
+        href: page.url
+      }, page.title);
+    }), m("button", {
+      onclick: ctrl.rotate
+    }, "Rotate links")
+  ];
+};
+
+m.module(document, app);
 
 //# sourceMappingURL=app.js.map
--- gofish_project/static/src/app.coffee
+++ gofish_project/static/src/app.coffee
@@ -1,4 +1,38 @@
-f = -> 'stuff'
+#namespace
+app = {}
 
-b = -> 'moar'
+#model
+app.PageList = -> m.prop [
+    {
+        title: "Getting Started",
+        url: "getting-started.html"
+    },
+    {
+        title: "Documentation",
+        url: "mithril.html"
+    },
+    {
+        title: "Mithril Blog",
+        url: "http://lhorie.github.io/mithril-blog/"
+    },
+    {
+        title: "Mailing List",
+        url: "https://groups.google.com/forum/#!forum/mithriljs"
+    }
+]
 
+#controller
+app.controller = ->
+    pages = app.PageList()
+
+    pages: pages,
+    rotate: -> pages().push(pages().shift())
+
+#view
+app.view = (ctrl) -> [
+    ctrl.pages().map((page) ->  m("a", {href: page.url}, page.title)),
+    m("button", {onclick: ctrl.rotate}, "Rotate links")
+]
+
+#initialize
+m.module(document, app)
