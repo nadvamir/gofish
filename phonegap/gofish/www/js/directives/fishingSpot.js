d7b559b2ccddaa044505b0157e79115c57dc08ec
Sean Jakobsen <skwjakobsen@gmail.com>
2015-02-06 21:45:26 +0000

    Added depth cues for moving left and right. Currently textual. Removed a couple unused files.

--- phonegap/gofish/www/js/directives/fishingSpot.js
+++ phonegap/gofish/www/js/directives/fishingSpot.js
@@ -9,21 +9,37 @@ goFish.directive("fishingSpot", [function(){
 			$scope.updateFishingSpot = function() {
 				var currentLevel = GameService.getCurrentLevel();
 				if (currentLevel.level && (currentLevel.level.position >= 0) && currentLevel.level.map && currentLevel.cues) {
+					// Update variables
 					$scope.position = currentLevel.level.position;
 					$scope.map = currentLevel.level.map[0];
 					$scope.cues = currentLevel.cues;
+
 					// Generate an array to traverse with ng-repeat to draw depth levels
 					var depth = $scope.map[$scope.position];
 					$scope.currentDepth = [];
 					for (var i = 1; i <= depth; i++) {
 						$scope.currentDepth.push(i);
 					};
-					/*
-					console.log("position");
-					console.log($scope.position);
-					console.log("depth");
-					console.log(depth);
-					*/
+
+					// Check left and right depths to draw some depth cues for left and right
+					// Draw depth cues one depth lower than can fish
+					$scope.cueDepth = depth + 1;
+					// Left
+					if ($scope.position == 0 || $scope.map[$scope.position - 1] < depth) {
+						$scope.leftDepthCue = "up";
+					} else if ($scope.map[$scope.position - 1] > depth) {
+						$scope.leftDepthCue = "down";
+					} else {
+						$scope.leftDepthCue = "level";
+					}
+					// Right
+					if ($scope.position == 19 || $scope.map[$scope.position + 1] < depth) {
+						$scope.rightDepthCue = "up";
+					} else if ($scope.map[$scope.position + 1] > depth) {
+						$scope.rightDepthCue = "down";
+					} else {
+						$scope.rightDepthCue = "level";
+					}
 				}
 				else {
 					console.log("ERROR finding necessary information for rendering fishing spot");
@@ -43,6 +59,9 @@ goFish.directive("fishingSpot", [function(){
 			$scope.position = 0;
 			$scope.cues = null;
 			$scope.currentDepth = [];
+			$scope.leftDepthCue = null;
+			$scope.rightDepthCue = null;
+			$scope.cueDepth = 11;
 		},
 		controllerAs: "fishingSpotCtrl"
 	};
