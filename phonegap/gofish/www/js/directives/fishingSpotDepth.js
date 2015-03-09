597e5b83a005a3cd3bd3f4970bf110532208c9b1
Sean Jakobsen <skwjakobsen@gmail.com>
2015-03-09 23:08:08 +0000

    Fish cues and HUD fix Draw fish hints from cues. Currently using 6 images. Fixed issue where arrows were drawn with a container that was blocking the top of the fishing spot from being clickable to fish.

--- phonegap/gofish/www/js/directives/fishingSpotDepth.js
+++ phonegap/gofish/www/js/directives/fishingSpotDepth.js
@@ -9,7 +9,21 @@ goFish.directive("fishingSpotDepth", [function(){
 			cues: "="
 		},
 		controller: function($scope, GameService) {
-			
+
+			var processCues = function() {
+				$scope.cue = null;
+				if ($scope.cues[0] > 5) {
+					$scope.cue = 6;
+				} else if ($scope.cues[0] > 0) {
+					$scope.cue = $scope.cues[0];
+				}
+			};
+
+			$scope.$watch('$scope.cues', function() {
+       			processCues();
+   			});
+
+   			processCues();
 		},
 		controllerAs: "fishingSpotDepthCtrl"
 	};
